1038b51366a57d3bb990406eb63803636ff43f4a
myrna90 <myrnamares90@gmail.com>
2019-03-13 17:15:59 -0600

    nombre funcion mostrar

--- src/data.js
+++ src/data.js
@@ -1,14 +1,12 @@
-/*window.movies = {
+window.movies = {
 
-    randomImageMovie: function(movies){ //peliculas debe ser un array
-        
-        const recomendedMovies= movies.length;
-        let aleatorio=Math.round(Math.random()*(recomendedMovies-1));
+    randomImageMovie: function(movies) { //peliculas debe ser un array
+
+        const recomendedMovies = movies.length;
+        let aleatorio = Math.round(Math.random() * (recomendedMovies - 1));
         let randomMovies = movies[aleatorio];
         return randomMovies;
     }
 
 
-}*/
-
-
+}
--- src/main.js
+++ src/main.js
@@ -1,22 +1,21 @@
-const mistery =  ["tt0482571", "tt0209144", "tt0120689", "tt1130884", "tt0114814", "tt0167404", "tt2267998", "tt1201607", "tt0440963"];
-const romance = ["tt0109830", "tt0120338", "tt0338013", "tt0119217", "tt1010048", "tt0211915", "tt1045658", "tt0118799", "tt0107048",  "tt0421715"];
-const animation = ["tt0101414","tt0298148","tt0398286","tt0103639","tt0121164","tt0097757","tt0119282","tt0120855","tt0892791","tt5311514","tt0104652","tt2401878"];
+const mistery = ["tt0482571", "tt0209144", "tt0120689", "tt1130884", "tt0114814", "tt0167404", "tt2267998", "tt1201607", "tt0440963"];
+const romance = ["tt0109830", "tt0120338", "tt0338013", "tt0119217", "tt1010048", "tt0211915", "tt1045658", "tt0118799", "tt0107048", "tt0421715"];
+const animation = ["tt0101414", "tt0298148", "tt0398286", "tt0103639", "tt0121164", "tt0097757", "tt0119282", "tt0120855", "tt0892791", "tt5311514", "tt0104652", "tt2401878"];
 const drama = ["tt0111161", "tt0468569", "tt0137523", "tt0110912", "tt0109830", "tt0120737", "tt0068646", "tt0816692", "tt0172495"];
 
 const arrMovies = [];
 
-function getArr (arr) {
+function getArr(arr) {
 
     const arrMovies = [];
-            for(let i = 0; i < arr.length; i++){
-                fetch('http://www.omdbapi.com/?i=' + arr[i] + '&apikey=691df107').then( (data) => {
-                return data.json();
-                }).then((dataAsJSON) => {
-                arrMovies[i] = dataAsJSON;
-                });
-            }
-        return arrMovies
+    for (let i = 0; i < arr.length; i++) {
+        fetch('http://www.omdbapi.com/?i=' + arr[i] + '&apikey=691df107').then((data) => {
+            return data.json();
+        }).then((dataAsJSON) => {
+            arrMovies[i] = dataAsJSON;
+        });
+    }
+    return arrMovies
 }
 
- console.log(getArr(drama));
-
+console.log(getArr(mistery));
